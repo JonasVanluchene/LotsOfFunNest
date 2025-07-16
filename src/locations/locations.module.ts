05ff255a2e673d172235207ded8b6c915674e5a2
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-07-16 10:38:30 +0200

    Add protection to locations endpoint

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
